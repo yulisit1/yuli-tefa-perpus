8fdc802fb57e70b6fd4ebfb82dccaaa583100e1e
yulisit1 <141709583+yulisit1@users.noreply.github.com>
2024-04-29 15:15:30 +0700

    Add files via upload

---
+++ components/Statistik.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+</template>
+
+<script setup>
+import Chart from 'chart.js/auto';
+
+const labels=[
+    'january',
+    'february',
+    'march',
+    'april',
+    'may',
+    'june',
+];
+
+const data= {
+    labels : labels,
+    datasets: [{
+        label: 'Pengunjung',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+};
+
+onMounted(()=>{
+    const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
+})
+</script>
---
+++ pages/buku/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-conter my-4">BUKU</h2>
+        <div class="my-3">
+          <input type="search" class="form-control rounded-5" placeholder="Mau baca apa hari ini?">
+        </div>
+        <div class="my-3 text-muted">menampilkan 3 dari 3</div>
+        <div class="row">
+          <div class="col-lg-2">
+            <div class="card-mb-3">
+              <div class="card-body">
+                <img src="~/assets/img/angkasa.png" class="cover" alt="cover 1">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-2">
+            <div class="card mb-3">
+              <div class="card-body">
+                <img src="~/assets/img/bucin.png" class="cover" alt="cover 2">
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-2">
+            <div class="card mb-3">
+              <div class="card-body">
+                <img src="~/assets/img/172.png" class="cover" alt="cover3">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card-body {
+  width: 100%;
+  height: 20em;
+  padding: 0;
+}
+.cover {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: 0 30;
+}
+</style>
